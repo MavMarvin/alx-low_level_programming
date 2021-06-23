35f18e6ec791181e4165b4b296b7ca46543b71a0
MavMarvin <marvinogwal@gmail.com>
2021-06-23 20:33:05 +0300

    10 Alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - Prints the alphabet in lowercase 10 times.
- * 
+ * Description: Print alphabet 10 times
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
