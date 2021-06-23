8a5433196b6dec0c8d17ab15eebaacec16e2e944
MavMarvin <marvinogwal@gmail.com>
2021-06-23 21:15:17 +0300

    Check Lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= a && c <= z)
+	{
+		return 1;
+	}else
+	{
+		return 0;
+	}
+	_putchar('\n');
+}
