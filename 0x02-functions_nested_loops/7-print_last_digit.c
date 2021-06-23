27cc3c95ae52eba44607b50b78d914daabe2ebd9
MavMarvin <marvinogwal@gmail.com>
2021-06-23 22:19:20 +0300

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Last digit
+ * @n: input number as an integer.
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
