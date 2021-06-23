42718ad46c5febc003123d76e786809b685431e1
MavMarvin <marvinogwal@gmail.com>
2021-06-23 21:51:22 +0300

    Check Signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Checks for sign of numbers
+ * @c: The character in ASCII code
+ * Return: Different values
+ */
+int print_sign(int n)
+{
+	if (A > 0)
+        	return (1);
+        else if (A == 0)
+        	return (0);
+    	else if (A < 0)
+        	return (-1);
+    	_putchar('\n');
+}
