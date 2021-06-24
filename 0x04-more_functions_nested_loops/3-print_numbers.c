00dd87d5f93a43cdb495b96fed81040f35db4d74
MavMarvin <marvinogwal@gmail.com>
2021-06-24 23:25:05 +0300

    Print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
+ * print_numbers - prints  0 to 9.
+ * Return: Always 0
  */
 void print_numbers(void)
 {
