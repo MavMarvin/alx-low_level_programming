6a7bcc24cb8707ab0b89c71b1287b270995cd5ea
MavMarvin <marvinogwal@gmail.com>
2021-07-04 13:11:58 +0300

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,9 @@ void rev_string(char *s)
 	int i, c, l;
 	char h;
 
-	//Find the string length where s[i] is end of string
 	for (i = 0; s[i] != '\0'; i++)
 		l = i;
 
-	//swap the values
 	for (i--, c = 0; c < l / 2; i--, c++)
 	{
 		h = s[c];
