a7d20ef8b97f4c0490f55b4b768b50dccd52f8f6
MavMarvin <marvinogwal@gmail.com>
2021-07-07 20:38:30 +0300

    Print String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	return ("Betty Holberton\n");
+}
