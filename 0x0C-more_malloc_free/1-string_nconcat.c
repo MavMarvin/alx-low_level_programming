9044abc9909d02095ace5af1e93bc2922198feaf
MavMarvin <marvinogwal@gmail.com>
2021-07-15 22:59:17 +0300

    Malloc Concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,52 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
 #include <stdio.h>
+
 /**
-* string_nconcat - concatenates two strings.
-* @s1: input array 1
-* @s2: input array 2
-* @n: positions to copy
-* Return: pointer to array
-*/
+ * string_nconcat - concatenate 2 strings, only n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes to include of s2
+ * Return: NULL if fail, else pointer to malloc memory
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *p;
-unsigned int size1 = 0, size2 = 0, i, j;
+	char *p;
+	int strlen1, i, c;
 
 	if (s1 == NULL)
-	{ s1 = ""; }
+		s1 = "";
 	if (s2 == NULL)
-	{ s2 = ""; }
-	while (s1[size1] != '\0')
-	{
-		size1++;
-	}
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
-	if (n > size2)
-	n = size2;
-	p = malloc((size1 + n + 1) * sizeof(char));
+		s2 = "";
+
+	strlen1 = (unsigned int)_strlen(s1);
+	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
-	return (NULL);
-	for (i = 0; i < (size1 + n) && s1[i] != '\0'; i++)
+		return (NULL);
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
 	{
-		p[i] = s1[i];
-	}
-	for (j = 0; i < (size1 + n) && s2[j] != '\0'; j++)
-	{
-		p[i] = s2[j];
-		i++;
+		if (i < strlen1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[c++];
 	}
 	p[i] = '\0';
-return (p);
+
+	return (p);
+}
+
+/**
+ * _strlen - find length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
