c9cff53c44ae16572e16b7041d5f29af27c70194
MavMarvin <marvinogwal@gmail.com>
2021-07-21 01:37:45 +0300

    Int array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,20 +1,24 @@
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
- * array_range - create an array of integers inclusive of min and max
- * @min: min value to include
- * @max: max value to include
- * Return: pointer to newly created array
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ *
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
  */
-
 int *array_range(int min, int max)
 {
-	int *p, i;
+	int *p;
+	int i;
 
 	if (min > max)
 		return (NULL);
 
-	p = malloc((max - min + 1) * sizeof(*p));
+	p = malloc(sizeof(*p) * ((max - min) + 1));
+
 	if (p == NULL)
 		return (NULL);
 
