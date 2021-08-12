80e940dec1bcb976990cfe5cc8e4b9664d457857
MavMarvin <marvinogwal@gmail.com>
2021-08-13 02:33:41 +0300

    Read and Print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,31 @@
 #include "holberton.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
- */
+  * read_textfile - ...
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: ...
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
+
+	if (!buff)
+		return (0);
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	free(buff);
 	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	return (readed);
 }
